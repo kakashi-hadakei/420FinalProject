3f2b67d84b0d2e5ea5bd6b7f39d9081492897584
kakashi-hadakei <963136786@qq.com>
2017-04-14 07:59:19 -0500

    added READ_CONTACT permission

--- BusinessCard/app/src/main/java/com/example/ece420final/businesscard/DetectionActivity.java
+++ BusinessCard/app/src/main/java/com/example/ece420final/businesscard/DetectionActivity.java
@@ -68,11 +68,6 @@ public class DetectionActivity extends AppCompatActivity  {
     private Bitmap getProcessedBitmap(String imgPath){
         if(imgPath != null) {
             Bitmap myBitmap = BitmapFactory.decodeFile(imgPath);
-            //~/420FinalProject/test.py
-            //myBitmap = adjustedContrast(myBitmap,1);
-
-
-
 
              /*gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # grayscale
                 _,thresh = cv2.threshold(gray,150,255,cv2.THRESH_BINARY_INV) # threshold
@@ -141,59 +136,6 @@ public class DetectionActivity extends AppCompatActivity  {
         }
     }
 
-    private Bitmap adjustedContrast(Bitmap src, double value)
-    {
 
-        // image size
-        int width = src.getWidth();
-        int height = src.getHeight();
-        // create output bitmap
-
-
-        // create a mutable empty bitmap
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-
-        // create a canvas so that we can draw the bmOut Bitmap from source bitmap
-        Canvas c = new Canvas();
-        c.setBitmap(bmOut);
-
-        // draw bitmap to bmOut from src bitmap so we can modify it
-        c.drawBitmap(src, 0, 0, new Paint(Color.BLACK));
-
-
-        // color information
-        int A, R, G, B;
-        int pixel;
-        // get contrast value
-        double contrast = Math.pow((100 + value) / 100, 2);
-
-        // scan through all pixels
-        for(int x = 0; x < width; ++x) {
-            for(int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                // apply filter contrast for every channel R, G, B
-                R = Color.red(pixel);
-                R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(R < 0) { R = 0; }
-                else if(R > 255) { R = 255; }
-
-                G = Color.green(pixel);
-                G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(G < 0) { G = 0; }
-                else if(G > 255) { G = 255; }
-
-                B = Color.blue(pixel);
-                B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(B < 0) { B = 0; }
-                else if(B > 255) { B = 255; }
-
-                // set new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
-            }
-        }
-        return bmOut;
-    }
 
 }
